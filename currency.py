b4c47dd4f78aabeff5e5eb734a144bd11c7d8eee
Nandu760 <kumarirajnandani188@gmail.com>
2025-06-18 12:08:18 +0530

    Add files via upload

---
+++ currency.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('currency.html', result='')
+
+@app.route('/convert', methods=['POST'])
+def convert():
+    amount = float(request.form['amount'])
+    from_currency = request.form['from_currency']
+    to_currency = request.form['to_currency']
+
+    try:
+        url = f"https://api.exchangerate.host/convert?from={from_currency}&to={to_currency}&amount={amount}"
+        response = requests.get(url)
+        data = response.json()
+        converted_amount = data['result']
+        result = f"{amount} {from_currency} = {converted_amount:.2f} {to_currency}"
+    except Exception as e:
+        result = "Error fetching data."
+        print("Error:", e)
+
+    return render_template('currency.html', result=result)
+    
+if __name__ == '__main__':
+    app.run(port=5002,debug=True)
